a03376bdf535dc43af704f33c48b3d7e142bcc24
Karol Bulwin <karolbulwin@gmail.com>
2019-02-02 11:01:59 +0100

    app init

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
+
+const app = express();
+const port = process.env.port || 3999;
+
+app.use(morgan('dev'));
+app.use(sassMiddleware({
+  src: path.join(__dirname),
+  dest: path.join(__dirname),
+  debug: true,
+  // outputStyle: 'compressed',
+  indentedSyntax: true,
+  sourceMap: true,
+}));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+  res.sendFile('/views/', 'index.html');
+});
+
+app.listen(port, () => {
+  debug(`Listening on ${port}`);
+});
