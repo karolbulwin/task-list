2f63b1038a0c3781125c206ba4785ecc81d2c905
Karol Bulwin <karolbulwin@gmail.com>
2019-02-16 11:00:48 +0100

    cache external url

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-const CACHE_NAME = 'task-list-cache-v1.2';
+const CACHE_NAME = 'task-list-cache-v1.3';
 const urlsToCache = [
   '/',
   'tasks.html',
   'favicon.ico',
   'public/stylesheets/style.css',
   'public/javascripts/main.js',
-  'public/javascripts/for-sw.js'
+  'public/javascripts/for-sw.js',
+  'https://fonts.googleapis.com/css?family=Open+Sans',
+  'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+  'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
 ];
 
 self.addEventListener('install', (event) => {
@@ -22,7 +28,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['task-list-cache-v1.2'];
+  const cacheWhitelist = ['task-list-cache-v1.3'];
 
   event.waitUntil(
     caches.keys().then(cacheNames => Promise.all(
