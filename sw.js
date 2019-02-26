c84c4b3d8e16231bc067f201c449ade43f5b4237
Karol Bulwin <karolbulwin@gmail.com>
2019-02-26 15:34:33 +0100

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-list-cache-v1.3.7';
+const CACHE_NAME = 'task-list-cache-v1.3.8';
 const urlsToCache = [
   '/',
   'tasks.html',
@@ -28,7 +28,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['task-list-cache-v1.3.7'];
+  const cacheWhitelist = ['task-list-cache-v1.3.8'];
 
   event.waitUntil(
     caches.keys().then(cacheNames => Promise.all(
