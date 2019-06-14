255f793383da5fe26361848772d314f729a579fe
Karol Bulwin <karolbulwin@gmail.com>
2019-06-14 23:12:37 +0200

    get button ele

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -326,11 +326,11 @@
           removeTaskFromList(task);
         }
         if (ev.target.tagName === 'I') {
-          const task = ev.target.parentElement;
+          const task = ev.target.parentElement.parentElement;
           editTaks(task);
         }
         if (ev.target.tagName === 'SPAN' && ev.target.innerText === '') {
-          const task = ev.target.parentElement;
+          const task = ev.target.parentElement.parentElement;
           editTaks(task);
         }
 
