1dd9ac0890d4e1ddca4fb6238e4369a273ed104e
Karol Bulwin <karolbulwin@gmail.com>
2019-06-15 08:58:35 +0200

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-list-cache-v1.4.5';
+const CACHE_NAME = 'task-list-cache-v1.4.6';
 const urlsToCache = [
   '/',
   'tasks.html',
@@ -8,10 +8,10 @@ const urlsToCache = [
   'public/javascripts/for-sw.js',
   'https://fonts.googleapis.com/css?family=Open+Sans',
   'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
   'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
 ];
 
 self.addEventListener('install', (event) => {
@@ -27,7 +27,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['task-list-cache-v1.4.5'];
+  const cacheWhitelist = ['task-list-cache-v1.4.6'];
 
   event.waitUntil(
     caches.keys().then(cacheNames => Promise.all(
